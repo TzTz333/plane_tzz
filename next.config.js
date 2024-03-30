1ce6026c17021d05ec46e8715bb0ed0543c5f741
TzTz333 <2296831370@qq.com>
2024-03-30 16:38:49 +0800

    feat: init next.js project

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const { withSentryConfig } = require("@sentry/nextjs");
+const path = require("path");
+
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    domains: [
+      "vinci-web.s3.amazonaws.com",
+      "planefs-staging.s3.ap-south-1.amazonaws.com",
+      "planefs.s3.amazonaws.com",
+      "images.unsplash.com",
+    ],
+  },
+  output: "standalone",
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
+};
+
+if (parseInt(process.env.NEXT_PUBLIC_ENABLE_SENTRY || "0")) {
+  module.exports = withSentryConfig(nextConfig, { silent: true }, { hideSourceMaps: true });
+} else {
+  module.exports = nextConfig;
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+module.exports = {
+  content: ["./pages/**/*.tsx", "./components/**/*.tsx", "./layouts/**/*.tsx", "./ui/**/*.tsx"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        theme: "#3f76ff",
+        "hover-gray": "#f5f5f5",
+        primary: "#f9fafb", // gray-50
+        secondary: "white",
+      },
+      keyframes: {
+        leftToaster: {
+          "0%": { left: "-20rem" },
+          "100%": { left: "0" },
+        },
+        rightToaster: {
+          "0%": { right: "-20rem" },
+          "100%": { right: "0" },
+        },
       },
     },
+    fontFamily: {
+      custom: ["Inter", "sans-serif"],
+    },
   },
-  plugins: [],
 };
-export default config;
